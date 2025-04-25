297f7ec6b6854177cb32265f554359b7c57c88a2
AdilMulimani <adilmulimani@gmail.com>
2025-04-25 23:22:08 +0530

    feat(src\interfaces): add auth.interface.ts

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,34 @@
+import {Request} from "express";
+
+export interface SignUpBody{
+name:string,
+email:string,
+password:string
+}
+
+export interface VerifyEmailBody {
+    verificationToken:string
+}
+
+export interface SignInBody{
+    email:string,
+    password:string
+}
+
+export interface ForgotPasswordBody{
+    email:string,
+}
+
+export interface VerifyResetTokenBody {
+    resetPasswordToken:string
+}
+
+export interface ResetPasswordBody {
+    resetPasswordToken:string,
+    password:string
+}
+
+export interface AuthRequest extends Request {
+    userId?: number;
+    token?:string
+}
